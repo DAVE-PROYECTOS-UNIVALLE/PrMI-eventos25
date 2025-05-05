0d4de7d2eca058638eb701c0358d2e6ac60dd7db
Davixo23 <david.utic@gmail.com>
2025-05-05 09:38:03 -0400

    evento on click

---
+++ app/src/main/java/com/example/eventos25/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.eventos25
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var btn1 : Button
+    private lateinit var tv1 : TextView
+    private lateinit var img1 : ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        btn1 = findViewById(R.id.btn1)
+        tv1 = findViewById(R.id.tv1)
+        img1 = findViewById(R.id.img3)
+
+        btn1.setOnClickListener {
+            Toast.makeText(this,
+                "Haz hecho click en el boton",
+                Toast.LENGTH_LONG).show()
+        }
+        tv1.setOnClickListener {
+            Toast.makeText(this,
+                "Haz hecho click en el textview",
+                Toast.LENGTH_LONG).show()
+        }
+        img1.setOnClickListener {
+            Toast.makeText(this,
+                "Haz hecho click en la imagen",
+                Toast.LENGTH_LONG).show()
+        }
+    }
+}
